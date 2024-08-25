922ede676273711c9c55303b738604bb0bf163eb
thekampany <mdekam@thekampany.com>
2024-08-25 14:31:59 +0200

    settings env

--- tripproject/settings.py
+++ tripproject/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-soeh8)%&&ondb+&u@e&)-!=kbl05q^1a39kb7*h!wi4&xa!iet'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
