f163090e5c017b598606ac50f6aa66117dee0c9b
thekampany <mdekam@thekampany.com>
2025-09-21 17:10:43 +0200

    new badge images

--- tripapp/management/commands/seed_global_badges.py
+++ tripapp/management/commands/seed_global_badges.py
@@ -6,19 +6,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         global_badges = [
-            {"name": "BingoBopper3", "image": "badges/bingobopper3.png", "threshold_value":"3", "threshold_type":"bingo_answer_uploads"},
-            {"name": "BingoBopper10", "image": "badges/bingobopper10.png", "threshold_value":"10", "threshold_type":"bingo_answer_uploads"},
-            {"name": "BingoBopper20", "image": "badges/bingobopper20.png", "threshold_value":"20", "threshold_type":"bingo_answer_uploads"},
-            {"name": "PlanesTrainsnBlogtypos3", "image": "badges/planestrainsnblogtypos3.png", "threshold_value":"3", "threshold_type":"log_entries"},
-            {"name": "PlanesTrainsnBlogtypos10", "image": "badges/planestrainsnblogtypos10.png", "threshold_value":"10", "threshold_type":"log_entries"},
-            {"name": "PlanesTrainsnBlogtypos20", "image": "badges/planestrainsnblogtypos20.png", "threshold_value":"20", "threshold_type":"log_entries"},
-            {"name": "Two Tripper", "image": "badges/twotripper.png", "threshold_value":"2", "threshold_type":"trip_count"},
-            {"name": "Three Tripper", "image": "badges/threetripper.png", "threshold_value":"3", "threshold_type":"trip_count"},
-            {"name": "Ten Tripper", "image": "badges/tentripper.png", "threshold_value":"10", "threshold_type":"trip_count"},
-            {"name": "Twenty Tripper", "image": "badges/twentytripper.png", "threshold_value":"20", "threshold_type":"trip_count"},
+            {"name": "BingoBopper3", "image": "badges/bingouploads3.png", "threshold_value":"3", "threshold_type":"bingo_answer_uploads"},
+            {"name": "BingoBopper10", "image": "badges/bingouploads10.png", "threshold_value":"10", "threshold_type":"bingo_answer_uploads"},
+            {"name": "BingoBopper20", "image": "badges/bingouploads20.png", "threshold_value":"20", "threshold_type":"bingo_answer_uploads"},
+            {"name": "PlanesTrainsnBlogtypos3", "image": "badges/blogswritten3.png", "threshold_value":"3", "threshold_type":"log_entries"},
+            {"name": "PlanesTrainsnBlogtypos10", "image": "badges/blogswritten10.png", "threshold_value":"10", "threshold_type":"log_entries"},
+            {"name": "PlanesTrainsnBlogtypos20", "image": "badges/blogswritten20.png", "threshold_value":"20", "threshold_type":"log_entries"},
+            {"name": "Two Tripper", "image": "badges/trips2.png", "threshold_value":"2", "threshold_type":"trip_count"},
+            {"name": "Three Tripper", "image": "badges/trips3.png", "threshold_value":"3", "threshold_type":"trip_count"},
+            {"name": "Ten Tripper", "image": "badges/trips10.png", "threshold_value":"10", "threshold_type":"trip_count"},
+            {"name": "Twenty Tripper", "image": "badges/trips20.png", "threshold_value":"20", "threshold_type":"trip_count"},
             {"name": "Track the Tripper", "image": "badges/trackthetripper.png", "threshold_value":"1", "threshold_type":"tripper_has_api_key"},
-            {"name": "My Log got 3 likes", "image": "badges/like3.png", "threshold_value":"3", "threshold_type":"log_likes"},
-            {"name": "My Log got 10 likes", "image": "badges/like10.png", "threshold_value":"10", "threshold_type":"log_likes"},
+            {"name": "My Log got 3 likes", "image": "badges/loglikes3.png", "threshold_value":"3", "threshold_type":"log_likes"},
+            {"name": "My Log got 10 likes", "image": "badges/loglikes10.png", "threshold_value":"10", "threshold_type":"log_likes"},
+            {"name": "My Log got 20 likes", "image": "badges/loglikes20.png", "threshold_value":"20", "threshold_type":"log_likes"},
         ]
 
         for badge_data in global_badges:
