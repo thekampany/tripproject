643a37f27a1caa44e08ea877217b255051d32310
thekampany <mdekam@thekampany.com>
2025-12-08 22:44:43 +0100

    migration37

--- tripapp/migrations/0037_dayprogram_map_image_dayprogram_overnight_location_and_more.py
+++ tripapp/migrations/0037_dayprogram_map_image_dayprogram_overnight_location_and_more.py
@@ -161,17 +161,4 @@ class Migration(migrations.Migration):
             name='scheduled_item',
             field=models.ForeignKey(blank=True, help_text='Optional: Associate this link with a specific ScheduledItem.', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='links', to='tripapp.scheduleditem'),
         ),
-        migrations.CreateModel(
-            name='LogEntryLike',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('emoji', models.CharField(max_length=10)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('logentry', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='likes', to='tripapp.logentry')),
-                ('tripper', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='logentry_likes', to='tripapp.tripper')),
-            ],
-            options={
-                'unique_together': {('logentry', 'tripper', 'emoji')},
-            },
-        ),
     ]
